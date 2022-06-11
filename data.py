5fc640e1b15373bea3f5b3b3cdc3f49548f65693
micah <micahjcfull@gmail.com>
2022-06-11 12:08:03 -0700

    typo

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
 """
 This module gets bitcoin network data from the internets if you don't have a bitcoin archive node.
--- tool_currency_converter.py
+++ tool_currency_converter.py
@@ -1,5 +1,5 @@
 # Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
 """
 This module is a stand-alone tool to help convert between dollars (or any fiat, really) and the bitcoins
